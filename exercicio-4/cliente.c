03cd9b4d3fd7ae0793769b3a756cc1d6feb4a553
Gustavo Waessman <gutawaessman@hotmail.com>
2024-09-24 17:51:19 -0300

    add logs

--- exercicio-4/cliente.c
+++ exercicio-4/cliente.c
@@ -12,6 +12,7 @@
 
 #define MAXLINE 4096
 
+// Função que cria um socket e verifica erros
 int Socket(int family, int type, int flags) {
     int sockfd;
     if ((sockfd = socket(family, type, flags)) < 0) {
@@ -27,33 +28,36 @@ int main(int argc, char **argv) {
     char   error[MAXLINE + 1];
     struct sockaddr_in servaddr;
 
-
+    // Verifica se os parâmetros de IP e porta foram passados corretamente
     if (argc != 3) {
-        strcpy(error,"uso: ");
-        strcat(error,argv[0]);
-        strcat(error," <IPaddress>");
+        strcpy(error, "uso: ");
+        strcat(error, argv[0]);
+        strcat(error, " <IPaddress>");
         perror(error);
         exit(1);
     }
 
     sockfd = Socket(AF_INET, SOCK_STREAM, 0);
 
+    // Configura o endereço do servidor
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    
     int port_arg = atoi(argv[2]);
     servaddr.sin_port   = htons((unsigned int)port_arg);
 
+    // Converte o IP fornecido para o formato binário
     if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
         perror("inet_pton error");
         exit(1);
     }
 
+    // Estabelece a conexão com o servidor
     if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
         perror("connect error");
         exit(1);
     }
 
+    // Obtém e imprime informações sobre o socket local e o servidor
     struct sockaddr_in local_addr;
     socklen_t addr_len = sizeof(local_addr);
 
@@ -62,51 +66,47 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-
     char ip_str[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(servaddr.sin_addr), ip_str, sizeof(ip_str));
-
     printf("Informações do servidor:\n");
     printf("IP: %s\n", ip_str);
     printf("Porta: %d\n\n\n", ntohs(servaddr.sin_port));
 
-
     inet_ntop(AF_INET, &(local_addr.sin_addr), ip_str, sizeof(ip_str));
-
     printf("Informações do socket local:\n");
     printf("IP: %s\n", ip_str);
     int porta = ntohs(local_addr.sin_port);
     printf("Porta: %d\n\n", porta);
 
-    for(;;){
-        while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+    // Loop de leitura de mensagens do servidor
+    for(;;) {
+        while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
             recvline[n] = 0;
-
             break;
         }
 
         char mensagem[5000];
 
-        if(strcmp(recvline, "ENCERRAR") == 0){
+        // Se o comando "ENCERRAR" for recebido, o cliente encerra
+        if (strcmp(recvline, "ENCERRAR") == 0) {
             printf("Recebido comando 'ENCERRAR', finalizando cliente\n");
             fflush(stdin);
             break;
         }
 
-        if(strcmp(recvline, "") != 0){
+        // Caso contrário, exibe a tarefa recebida e responde ao servidor
+        if (strcmp(recvline, "") != 0) {
             sprintf(mensagem, "Tarefa recebida: %s\n", recvline);
             printf("%s", mensagem);
 
             fflush(stdin);
-            sleep(3);
+            sleep(3);  // Simula a execução de uma tarefa
 
             sprintf(mensagem, "TAREFA_%s CONCLUIDA por %s / %d\n", recvline, ip_str, porta);
-
-
             write(sockfd, mensagem, strlen(mensagem));
         }
 
-        recvline[0] = '\0';
+        recvline[0] = '\0'; // Limpa a mensagem para a próxima leitura
     }
 
     exit(0);
--- exercicio-4/servidor.c
+++ exercicio-4/servidor.c
@@ -12,46 +12,51 @@
 
 #define LISTENQ 10
 #define MAXDATASIZE 100
-char arquivo[] = "logs.txt";
+char arquivo[] = "logs.txt"; // Arquivo de log
 
-
-int GetRandomNumber(int n){
-    return rand() % (n);
+// Função que retorna um número aleatório de 0 até n-1
+int GetRandomNumber(int n) {
+    return rand() % n;
 }
 
+// Função para criar um socket e verificar erros
 int Socket(int family, int type, int flags) {
     int sockfd;
     if ((sockfd = socket(family, type, flags)) < 0) {
         perror("socket");
         exit(1);
-    } else
+    }
     return sockfd;
 }
 
-void Getpeername(int connfd, struct sockaddr *__restrict__ client_addr, socklen_t *__restrict__ addrlen){
+// Envelopamento função Getpeername
+void Getpeername(int connfd, struct sockaddr *__restrict__ client_addr, socklen_t *__restrict__ addrlen) {
     if (getpeername(connfd, client_addr, addrlen) < 0) {
         perror("getpeername");
         exit(1);
     }
 }
 
-void Bind(int listenfd, const struct sockaddr *servaddr, socklen_t __len){
+// Envelopamento função Bind
+void Bind(int listenfd, const struct sockaddr *servaddr, socklen_t __len) {
     if (bind(listenfd, servaddr, __len) == -1) {
         perror("bind");
         exit(1);
     }
 }
 
-void Getsockname(int listenfd, struct sockaddr *__restrict__ servaddr, socklen_t *__restrict__ len){
+// Envelopamento função Getsockname
+void Getsockname(int listenfd, struct sockaddr *__restrict__ servaddr, socklen_t *__restrict__ len) {
     if (getsockname(listenfd, servaddr, len) == -1) {
         perror("getsockname");
         exit(1);
     }
 }
 
-int Accept(int listenfd, struct sockaddr *__restrict__ __addr, socklen_t *__restrict__ __addr_len){
+// Envelopamento função Accept
+int Accept(int listenfd, struct sockaddr *__restrict__ __addr, socklen_t *__restrict__ __addr_len) {
     int connfd;
-    if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
+    if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1) {
         perror("accept");
         exit(1);
     }
@@ -64,46 +69,57 @@ int main (int argc, char **argv) {
     struct sockaddr_in servaddr;
     char   buf[MAXDATASIZE], temp[MAXDATASIZE];
 
-    
+    // Lista de tarefas que podem ser enviadas para o cliente
     char *TAREFAS[] = {"LIMPEZA", "COZINHA", "ESTUDOS", "LOUÇA", "COMPRAS", "ENCERRAR"};
     int qtdTarefas = 6;
-    
+
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
+    // Configuração do endereço do servidor
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family      = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    // Conversão da porta passada como argumento
     int port_arg = atoi(argv[1]);
     servaddr.sin_port        = htons((unsigned int)port_arg);
 
     Bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
     socklen_t len = sizeof(servaddr);
+    // Obtém informações do socket
     Getsockname(listenfd, (struct sockaddr *)&servaddr, &len);
 
+    // Coloca o socket em modo de escuta
     if (listen(listenfd, LISTENQ) == -1) {
         perror("listen");
         exit(1);
     }
-    
+
+    // Loop principal para aceitar conexões
     for ( ; ; ) {
       
+        // Aceita uma nova conexão
         connfd = Accept(listenfd, (struct sockaddr *) NULL, NULL);
-
-        pid_t pid = fork();
+        
+        // Cria um processo filho para lidar com o cliente
+        pid_t pid = fork(); 
 
         if (pid < 0) {
             perror("fork");
         }
         if (pid == 0) {
-            // ChildProcess();
+            // Processo filho
+
+            // Inicializa a semente para gerar números aleatórios
             srand(time(NULL));
-            close(listenfd);
+            // Fecha o socket de escuta no processo filho
+            close(listenfd); 
 
             struct sockaddr_in client_addr;
             socklen_t addrlen = sizeof(client_addr);
 
+            // Obtém informações do cliente
             Getpeername(connfd, (struct sockaddr *)&client_addr, &addrlen);
 
             char ip_str_svr[INET_ADDRSTRLEN];
@@ -112,64 +128,56 @@ int main (int argc, char **argv) {
             char ip_str_cli[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, &(client_addr.sin_addr), ip_str_cli, sizeof(ip_str_cli));
 
+            // Log de conexão do cliente
             FILE *logFile = fopen(arquivo, "a");
-
             char logLine2[5000];
             sprintf(logLine2, "Cliente conectado IP/porta cliente: %s/%d\n", ip_str_cli, ntohs(client_addr.sin_port));
             fputs(logLine2, logFile);
             fclose(logFile);
 
+            // Envia tarefas aleatórias para o cliente até receber o comando "ENCERRAR"
             char logLineTarefa[100];
+            while(strcmp("ENCERRAR", temp)!=0) {
 
-            while(strcmp("ENCERRAR", temp)!=0){
-
-                int tarefa = GetRandomNumber(qtdTarefas);
+                // Seleciona uma tarefa aleatória
+                int tarefa = GetRandomNumber(qtdTarefas);  
                 logFile = fopen(arquivo, "a");
-                // printf("Enviando tarefa %s\n", TAREFAS[tarefa]);
+
+                // Log da tarefa enviada
                 sprintf(logLineTarefa, "Enviando tarefa %s\n", TAREFAS[tarefa]);
                 fputs(logLineTarefa, logFile);
                 fclose(logFile);
+
                 snprintf(temp, sizeof(buf), TAREFAS[tarefa]);
                 snprintf(buf, sizeof(buf), TAREFAS[tarefa]);
-                write(connfd, buf, strlen(buf));
+                // Envia a tarefa para o cliente
+                write(connfd, buf, strlen(buf)); 
+
                 int n;
-                while ( (n = read(connfd, buf, MAXDATASIZE-1)) > 0) {
+                while ((n = read(connfd, buf, MAXDATASIZE-1)) > 0) {
                     buf[n] = 0;
-                    // printf("Cliente finalizado:\n");
-                    // if (fputs(buf, stdout) == EOF) {
-                    //     perror("fputs error");
-                    //     exit(1);
-                    // }
 
+                    // Log da resposta do cliente
                     logFile = fopen(arquivo, "a");
-
                     sprintf(logLine2, "%s\n", buf);
                     fputs(logLine2, logFile);
-
-
                     fclose(logFile);
                     break;
                 }
             }
 
-
+            // Log de desconexão do cliente
             logFile = fopen(arquivo, "a");
             sprintf(logLine2, "Cliente finalizado IP/porta cliente: %s/%d\n", ip_str_cli, ntohs(client_addr.sin_port));
             fputs(logLine2, logFile);
             fclose(logFile);
 
-            // printf("IP/porta servidor: %s/%d\n", ip_str_svr, ntohs(servaddr.sin_port));
-            // printf("IP/porta cliente: %s/%d\n", ip_str_cli, ntohs(client_addr.sin_port));
-            // printf("Fechando filho!\n");
-
-            close(connfd);
-
+            close(connfd);  // Fecha a conexão com o cliente
             exit(0);
-        }
+        } 
         else {
-            //ParentProcess();
-            close(connfd);
-            // printf("Finalizando conexão no pai\n");
+            // Processo pai
+            close(connfd);  // Fecha o socket do cliente no processo pai
             fflush(stdin);
         }
     }
