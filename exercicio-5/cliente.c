bd903829bad3445e94221c8362b38a3926d92ecb
jose lucas <j155978@dac.unicamp.br>
2024-10-11 15:24:54 -0300

    added close() to client file

--- exercicio-5/cliente.c
+++ exercicio-5/cliente.c
@@ -102,18 +102,12 @@ int main(int argc, char **argv) {
 
     char ip_str[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(servaddr.sin_addr), ip_str, sizeof(ip_str));
-    // printf("Informações do servidor:\n");
-    // printf("IP: %s\n", ip_str);
-    // printf("Porta: %d\n\n\n", ntohs(servaddr.sin_port));
 
     inet_ntop(AF_INET, &(local_addr.sin_addr), ip_str, sizeof(ip_str));
-    // printf("Informações do socket local:\n");
     printf("clientId %d Conectado. IP: %s ", clientId, ip_str);
     int porta = ntohs(local_addr.sin_port);
     printf("Porta: %d\n", porta);
     
-    sleep(2);
-
     while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
         recvline[n] = 0;
         if (fputs(recvline, stdout) == EOF) {
@@ -121,6 +115,10 @@ int main(int argc, char **argv) {
             exit(1);
         }
     }
+
+    sleep(2);
+    close(sockfd);
+
     GetCurrentTime(hora, sizeof(hora));
     logFile = fopen(arquivo, "a");
     sprintf(logLine2, "%s : clienteId %d Finalizando!\n", hora, clientId);
