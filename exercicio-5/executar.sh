d11d8ba31fee8d720ed6328ad1c443903abf13d7
jose lucas <j155978@dac.unicamp.br>
2024-10-18 14:48:41 -0300

    made script more flexible

--- exercicio-5/executar.sh
+++ exercicio-5/executar.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Uso: $0 <porta>"
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Uso: $0 <porta> <número_de_conexões>"
     exit 1
 fi
 
-# Executa 15 instâncias do cliente com o parâmetro 495
-for i in {1..15}
+PORT=$1
+CONNECTIONS=$2
+
+# Executa o número especificado de instâncias do cliente
+for i in $(seq 1 $CONNECTIONS)
 do
-    ./cliente 0.0.0.0 "$1" "$i" &
+    ./cliente 0.0.0.0 "$PORT" "$i" &
 done
 
 # Opcional: espera que todos os processos filhos terminem
