d649942bee9678b45a99d2780c58ca7f6fa51746
jose lucas <j155978@dac.unicamp.br>
2024-11-05 20:21:11 -0300

    copied server file from example 8.15 TCP and UDP Echo Server Using select page 316

--- exercicio-7/servidor.c
+++ exercicio-7/servidor.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
+#include <sys/wait.h>
 
 
 #define LISTENQ 10
@@ -82,11 +83,58 @@ int main(int argc, char **argv) {
     const int on = 1;
     struct sockaddr_in cliaddr, servaddr;
     void sig_chld(int);
+    int port_arg = atoi(argv[1]);
 
     /* create listening TCP socket */
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);
     bzero(&servaddr, sizeof(servaddr));
+    
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERV_PORT);    
+    servaddr.sin_port = htons((unsigned int)port_arg);
+
+    Setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    Bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    Listen(listenfd, LISTENQ);
+
+    
+    /* create UDP socket */
+    udpfd = Socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons((unsigned int)port_arg);
+    Bind(udpfd, (struct sockaddr *)&servaddr, sizeof(servaddr)); 
+
+    Signal(SIGCHLD, sig_chld); /* must call waitpid() */
+
+    FD_ZERO(&rset);
+    maxfdp1 = max(listenfd, udpfd) + 1;
+    for ( ; ; ) {
+        FD_SET(listenfd, &rset);
+        FD_SET(udpfd, &rset);
+        if ( (nready = select(maxfdp1, &rset, NULL, NULL, NULL)) < 0) {
+            if (errno == EINTR)
+                continue; /* back to for() */
+            else
+                err_sys("select error");
+        }
+        
+        if (FD_ISSET(listenfd, &rset)) {
+            len = sizeof(cliaddr);
+            connfd = Accept(listenfd, (struct sockaddr *) &cliaddr, &len);
+            
+            if ( (childpid = Fork()) == 0) { /* child process */
+                Close(listenfd); /* close listening socket */
+                str_echo(connfd); /* process the request */
+                exit(0);
+            }
+            Close(connfd); /* parent closes connected socket */
+        }
+        if (FD_ISSET(udpfd, &rset)) { 
+            len = sizeof(cliaddr);
+            n = Recvfrom(udpfd, mesg, MAXLINE, 0, (struct sockaddr *) &cliaddr, &len);
+            Sendto(udpfd, mesg, n, 0, (struct sockaddr *) &cliaddr, len);
+        }
+    }           
 }
