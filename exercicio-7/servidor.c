6aa8997619dc68c157969879dd6f429a9a7e64d6
jose lucas <j155978@dac.unicamp.br>
2024-11-05 19:55:59 -0300

    created servidor file

---
+++ exercicio-7/servidor.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+
+#define LISTENQ 10
+#define MAXLINE 500
+
+// Função que retorna um número aleatório de 0 até n-1
+int GetRandomNumber(int n) {
+    return rand() % n;
+}
+
+// Função para criar um socket e verificar erros
+int Socket(int family, int type, int flags) {
+    int sockfd;
+    if ((sockfd = socket(family, type, flags)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    return sockfd;
+}
+
+// Envelopamento função Getpeername
+void Getpeername(int connfd, struct sockaddr *__restrict__ client_addr, socklen_t *__restrict__ addrlen) {
+    if (getpeername(connfd, client_addr, addrlen) < 0) {
+        perror("getpeername");
+        exit(1);
+    }
+}
+
+// Envelopamento função Bind
+void Bind(int listenfd, const struct sockaddr *servaddr, socklen_t __len) {
+    if (bind(listenfd, servaddr, __len) == -1) {
+        perror("bind");
+        exit(1);
+    }
+}
+
+// Envelopamento função Getsockname
+void Getsockname(int listenfd, struct sockaddr *__restrict__ servaddr, socklen_t *__restrict__ len) {
+    if (getsockname(listenfd, servaddr, len) == -1) {
+        perror("getsockname");
+        exit(1);
+    }
+}
+
+// Envelopamento função Accept
+int Accept(int listenfd, struct sockaddr *__restrict__ __addr, socklen_t *__restrict__ __addr_len) {
+    int connfd;
+    if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1) {
+        perror("accept");
+        exit(1);
+    }
+    return connfd;
+}
+
+// Envelopamento função Listen
+void Listen(int sockfd, int backlog) {
+    if (listen(sockfd, backlog) == -1) {
+        perror("listen");
+        exit(1);
+    }    
+}
+
+int main(int argc, char **argv) {
+    int listenfd, connfd, udpfd, nready, maxfdp1;
+    char mesg[MAXLINE];
+    pid_t childpid;
+    fd_set rset;
+    ssize_t n;
+    socklen_t len;
+    const int on = 1;
+    struct sockaddr_in cliaddr, servaddr;
+    void sig_chld(int);
+
+    /* create listening TCP socket */
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);    
+}
