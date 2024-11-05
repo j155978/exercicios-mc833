2e480a90161d55de05e361a2b7a5bf5eb13398b6
jose lucas <j155978@dac.unicamp.br>
2024-11-05 19:41:08 -0300

    erro

---
+++ exercicio-6/.nfs00000000016382c10000004d
@@ -0,0 +1,146 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAXLINE 4096
+
+// Função que cria um socket e verifica erros
+int Socket(int family, int type, int flags) {
+    int sockfd;
+    if ((sockfd = socket(family, type, flags)) < 0) {
+        perror("socket");
+        exit(1);
+    } else
+        return sockfd;
+}
+
+// Envelopamento função Getsockname
+void Getsockname(int listenfd, struct sockaddr *__restrict__ servaddr, socklen_t *__restrict__ len) {
+    if (getsockname(listenfd, servaddr, len) == -1) {
+        perror("getsockname");
+        exit(1);
+    }
+}
+
+// Envelopamento da função inet_pton
+void Inet_pton(int family, const char *restrict src, void *restrict dst) {
+    if (inet_pton(AF_INET, src, dst) <= 0) {
+        perror("inet_pton error");
+        exit(1);
+    }
+}
+
+// Envelopamento da função connect
+void Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    if (connect(sockfd, addr, addrlen) < 0) {
+        perror("connect error");
+        exit(1);
+    }    
+}
+
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+void cliente(FILE *fp, int sockfd){
+    int maxfdp1, stdineof;
+    fd_set rset;
+    char sendline[MAXLINE], recvline[MAXLINE];
+
+    stdineof = 0;
+
+    FD_ZERO(&rset);
+    for ( ; ; ){
+        if (stdineof == 0)
+            FD_SET(fileno(fp), &rset);
+        
+        FD_SET(sockfd, &rset);
+        maxfdp1 = max(fileno(fp), sockfd) + 1;
+        if (select(maxfdp1, &rset, NULL, NULL, NULL) < 0) {
+            perror("select error");
+            break;
+        }
+
+        if(FD_ISSET(sockfd, &rset)){ //Socket is readable
+            int qtd = read(sockfd, recvline, MAXLINE);
+            
+            if(qtd == 0){
+                if(stdineof == 1)
+                    return; //normal termination
+                else
+                    perror("str_cli: server terminated prematurely");
+            }
+            recvline[qtd] = '\0';
+            fputs(recvline, stdout);
+            char arquivolog[] = "saida.txt";
+            FILE *logFile = fopen(arquivolog, "a");
+            fputs(recvline, logFile);
+            fclose(logFile);
+        }
+
+        if(FD_ISSET(fileno(fp), &rset)){
+            while(fgets(sendline, MAXLINE, fp) != NULL){
+                write(sockfd, sendline, strlen(sendline));
+            }
+            stdineof = 1;
+            shutdown(sockfd, SHUT_WR); //send FIN
+            FD_CLR(fileno(fp), &rset);
+            continue;
+        } //input is readable
+    }
+}
+
+
+int main(int argc, char **argv) {
+    int sockfd[2]; // Array para dois sockets
+    struct sockaddr_in servaddr[2];
+
+    if (argc != 5) {
+        fprintf(stderr, "uso: %s <IPaddress1> <Port1> <IPaddress2> <Port2>\n", argv[0]);
+        exit(1);
+    }
+
+    // Criar e conectar os sockets para os dois servidores
+    for (int i = 0; i < 2; i++) {
+        sockfd[i] = Socket(AF_INET, SOCK_STREAM, 0);
+
+        memset(&servaddr[i], 0, sizeof(servaddr[i]));
+        servaddr[i].sin_family = AF_INET;
+        servaddr[i].sin_port = htons(atoi(argv[i * 2 + 2]));
+
+        Inet_pton(AF_INET, argv[i * 2 + 1], &servaddr[i].sin_addr);
+        Connect(sockfd[i], (struct sockaddr *)&servaddr[i], sizeof(servaddr[i]));
+    }
+
+    printf("Conexões estabelecidas! Você pode enviar mensagens para os dois servidores.\n");
+    printf("Digite 'exit' para encerrar.\n");
+
+
+    char arquivo[] = "entrada.txt";
+    
+
+    // Executa a função cliente para cada socket
+    for (int i = 0; i < 2; i++) {
+        printf("Conectando ao servidor %d...\n", i + 1);
+        FILE *logFile = fopen(arquivo, "r");
+        cliente(logFile, sockfd[i]);
+        fclose(logFile);
+    }
+
+    // Fechar sockets
+    for (int i = 0; i < 2; i++) {
+        close(sockfd[i]);
+    }
+
+    return 0;
+}
---
+++ exercicio-6/servidor.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+
+#define LISTENQ 10
+#define MAXDATASIZE 5000
+
+// Função que retorna um número aleatório de 0 até n-1
+int GetRandomNumber(int n) {
+    return rand() % n;
+}
+
+// Função para criar um socket e verificar erros
+int Socket(int family, int type, int flags) {
+    int sockfd;
+    if ((sockfd = socket(family, type, flags)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    return sockfd;
+}
+
+// Envelopamento função Getpeername
+void Getpeername(int connfd, struct sockaddr *__restrict__ client_addr, socklen_t *__restrict__ addrlen) {
+    if (getpeername(connfd, client_addr, addrlen) < 0) {
+        perror("getpeername");
+        exit(1);
+    }
+}
+
+// Envelopamento função Bind
+void Bind(int listenfd, const struct sockaddr *servaddr, socklen_t __len) {
+    if (bind(listenfd, servaddr, __len) == -1) {
+        perror("bind");
+        exit(1);
+    }
+}
+
+// Envelopamento função Getsockname
+void Getsockname(int listenfd, struct sockaddr *__restrict__ servaddr, socklen_t *__restrict__ len) {
+    if (getsockname(listenfd, servaddr, len) == -1) {
+        perror("getsockname");
+        exit(1);
+    }
+}
+
+// Envelopamento função Accept
+int Accept(int listenfd, struct sockaddr *__restrict__ __addr, socklen_t *__restrict__ __addr_len) {
+    int connfd;
+    if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1) {
+        perror("accept");
+        exit(1);
+    }
+    return connfd;
+}
+
+// Envelopamento função Listen
+void Listen(int sockfd, int backlog) {
+    if (listen(sockfd, backlog) == -1) {
+        perror("listen");
+        exit(1);
+    }    
+}
+
+void GetCurrentTime(char *hora, size_t tamanho) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    // Converte o tempo para a estrutura tm
+    struct tm *tm_info = localtime(&tv.tv_sec);
+
+    // Formata a data e hora com milissegundos
+    snprintf(hora, tamanho, "%04d-%02d-%02d %02d:%02d:%02d.%03ld",
+             tm_info->tm_year + 1900, 
+             tm_info->tm_mon + 1, 
+             tm_info->tm_mday, 
+             tm_info->tm_hour, 
+             tm_info->tm_min, 
+             tm_info->tm_sec, 
+             tv.tv_usec / 1000); // Converte microsegundos para milissegundos
+}
+
+void PrintServerDefaultMessage(char* buf, char* ipCliente, unsigned int portaCliente) {
+    time_t ticks;
+    int cpu = GetRandomNumber(100);
+    int memory = GetRandomNumber(100);
+    
+    ticks = time(NULL);
+    sprintf(buf, "IP: %s\nPorta: %d\nHorário: %sCPU: %d%%\nMemória: %d%%\nStatus: Ativo\n",
+        ipCliente,
+        portaCliente,
+        ctime(&ticks),
+        cpu,
+        memory
+    );
+    return;  
+}
+
+// Função para lidar com a comunicação de um cliente
+void handle_client(int connfd, char* ipCliente, unsigned int portaCliente) {
+    ssize_t n;
+    char line[MAXDATASIZE];
+
+    PrintServerDefaultMessage(line, ipCliente, portaCliente);
+    write(connfd, line, strlen(line));
+
+    while ((n = read(connfd, line, MAXDATASIZE - 1)) > 0) {
+        line[n] = '\0';  // Termina a string recebida
+        printf("Mensagem do cliente: %s\n", line);
+        
+        // Envia de volta a mensagem recebida para o cliente como confirmação
+        write(connfd, line, strlen(line));
+    }
+
+    if (n < 0) {
+        perror("Erro na leitura do socket");
+    }
+    printf("Cliente desconectado.\n");
+    close(connfd);  // Fecha o socket para este cliente
+}
+
+int main(int argc, char **argv) {
+    srand(time(NULL));
+    int listenfd, connfd;
+    struct sockaddr_in servaddr, clientaddr;
+
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <Port>\n", argv[0]);
+        exit(1);
+    }
+
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    int port_arg = atoi(argv[1]);
+    servaddr.sin_port = htons((unsigned int)port_arg);
+
+    Bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    Listen(listenfd, LISTENQ);
+
+    printf("Servidor aguardando conexões...\n");
+
+    for (;;) {
+        socklen_t clilen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (struct sockaddr *)&clientaddr, &clilen);
+        
+        Getpeername(connfd, (struct sockaddr *)&clientaddr, &clilen);
+
+        char ipCliente[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(clientaddr.sin_addr), ipCliente, sizeof(ipCliente));
+        unsigned int portaCliente = ntohs(clientaddr.sin_port);
+
+        printf("Cliente conectado.\n");
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            close(listenfd);
+            handle_client(connfd, ipCliente, portaCliente);
+            exit(0);
+        } else if (pid > 0) { 
+            close(connfd); 
+        } else {
+            perror("Erro no fork");
+            close(connfd);
+        }
+
+        while (waitpid(-1, NULL, WNOHANG) > 0) {
+            
+        }
+    }
+    return 0;
+}
